32eb6eed5d86c02f42b3db7620daa0480e6d66f0
pekr0002 <pekr0002@stud.kea.dk>
2025-11-05 09:47:40 +0100

    heroText - tailwind

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,9 +1,11 @@
 const HeroText = () => {
   return (
-    <article>
-      <h1>The Perfect Moment</h1>
-      <h2> Between Past And Future</h2>
-      <button>Buy Now</button>
+    <article className="font-[Poppins] text-[64px] leading-20 tracking-[1.28px] text-white">
+      <h1 className="font-bold">The Perfect Moment</h1>
+      <h2 className="font-normal"> Between Past And Future</h2>
+      <button className="w-[250px] h-[70px] shrink-0 rounded-full border-3 border-white bg-transparent text-[#fffafa] font-[Poppins] text-2xl font-medium">
+        <p>Buy Now</p>
+      </button>
     </article>
   );
 };
